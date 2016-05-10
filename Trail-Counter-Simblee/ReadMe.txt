548d7a5fc75438e4e7cefaa27d8b4cfeb05ccc2c
chipmc <chip@mcclellands.org>
2016-05-10 09:14:00 +0800

    Initial Commit

---
+++ Trail-Counter-Simblee/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Trail-Counter-Simblee
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Trail-Counter-Simblee
+  Created by Charles McClelland on 5/10/16
+  Copyright © 2016 Charles McClelland
+  Licence GNU General Public Licence
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2016
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ Trail-Counter-Simblee/Trail_Counter_Simblee.ino
@@ -0,0 +1,100 @@
+///
+/// @mainpage	Trail-Counter-Simblee
+///
+/// @details	This is the Simblee side of the trail counter
+/// @n
+/// @n
+/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
+///
+/// @author		Charles McClelland
+/// @author		Charles McClelland
+/// @date		5/10/16 9:13 AM
+/// @version	<#version#>
+///
+/// @copyright	(c) Charles McClelland, 2016
+/// @copyright	GNU General Public Licence
+///
+/// @see		ReadMe.txt for references
+///
+
+
+///
+/// @file		Trail_Counter_Simblee.ino
+/// @brief		Main sketch
+///
+/// @details	<#details#>
+/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
+///
+/// @author		Charles McClelland
+/// @author		Charles McClelland
+/// @date		5/10/16 9:13 AM
+/// @version	<#version#>
+///
+/// @copyright	(c) Charles McClelland, 2016
+/// @copyright	GNU General Public Licence
+///
+/// @see		ReadMe.txt for references
+/// @n
+///
+
+
+// Core library for code-sense - IDE-based
+#if defined(WIRING) // Wiring specific
+    #include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+    #include "WProgram.h"
+#elif defined(ROBOTIS) // Robotis specific
+    #include "libpandora_types.h"
+    #include "pandora.h"
+#elif defined(MPIDE) // chipKIT specific
+    #include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+    #include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+    #include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+    #include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+    #include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+    #include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+    #include "Arduino.h"
+#elif defined(RFDUINO) // RFduino specific
+    #include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+    #include "application.h"
+#elif defined(ESP8266) // ESP8266 specific
+    #include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+    #include "Arduino.h"
+#else // error
+    #   error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Add setup code
+void setup()
+{
+
+}
+
+// Add loop code
+void loop()
+{
+
+}
